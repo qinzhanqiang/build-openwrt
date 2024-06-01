391cf0f1fdd235bc3445b4e2d5f41ce7109acd9b
qinzhanqiang <1060371831@qq.com>
2024-06-01 19:59:07 +0800

    add mt798x

---
+++ config/immortalwrt-mt798x-master/diy-part1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt
+# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
+#========================================================================================================================
+
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# sed -i '$a src-git ysx88 https://github.com/ysx88/openwrt-packages' feeds.conf.default
+
+
+# other
+# rm -rf package/emortal/{autosamba,ipv6-helper}
+
